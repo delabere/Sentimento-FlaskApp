9b8c37ad6cd0762019192b4083a9030b0b0c4fb1
delabere <jack.rickards@hotmail.co.uk>
2019-11-21 23:21:52 +0000

    got parser working and created rugby_sentiment_data

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,6 +6,15 @@ import os
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'credentials.json'
 
+client = language_v1.LanguageServiceClient()
+# Available types: PLAIN_TEXT, HTML
+type_ = enums.Document.Type.PLAIN_TEXT
+language = "en"
+encoding_type = enums.EncodingType.UTF8
+
+
+
+
 
 def sample_analyze_entity_sentiment(text_content, unique_id):
     """
@@ -18,21 +27,21 @@ def sample_analyze_entity_sentiment(text_content, unique_id):
 
     entity_data = []
 
-    client = language_v1.LanguageServiceClient()
+    # client = language_v1.LanguageServiceClient()
 
     # text_content = 'Grapes are good. Bananas are bad.'
 
-    # Available types: PLAIN_TEXT, HTML
-    type_ = enums.Document.Type.PLAIN_TEXT
+    # # Available types: PLAIN_TEXT, HTML
+    # type_ = enums.Document.Type.PLAIN_TEXT
 
     # Optional. If not specified, the language is automatically detected.
     # For list of supported languages:
     # https://cloud.google.com/natural-language/docs/languages
-    language = "en"
+    # language = "en"
     document = {"content": text_content, "type": type_, "language": language}
 
     # Available values: NONE, UTF8, UTF16, UTF32
-    encoding_type = enums.EncodingType.UTF8
+    # encoding_type = enums.EncodingType.UTF8
 
     response = client.analyze_entity_sentiment(document, encoding_type=encoding_type)
     # Loop through entities returned from the API
@@ -48,10 +57,13 @@ zipped_data = zip(source_data['url'], source_data['description'])
 
 index = 0
 for unique_id, description in zipped_data:
-    entity_data =  pd.DataFrame(sample_analyze_entity_sentiment(description, unique_id))
     index += 1
-    print(f'{index} / {len(source_data)}')
-    if not os.path.exists(r'data/rugby_sentiment_data.csv'):
-        entity_data.to_csv(r'data/rugby_sentiment_data.csv', index=False)
+    try:
+        entity_data =  pd.DataFrame(sample_analyze_entity_sentiment(description, unique_id))
+        print(f'{index} / {len(source_data)}')
+        if not os.path.exists(r'data/rugby_sentiment_data.csv'):
+            entity_data.to_csv(r'data/rugby_sentiment_data.csv', index=False)
 
-    entity_data.to_csv(r'data/rugby_sentiment_data.csv', mode='a', header=False ,index=False)
+        entity_data.to_csv(r'data/rugby_sentiment_data.csv', mode='a', header=False ,index=False)
+    except:
+        pass
