2f4e1ec88b3f3335a0bb258de94525154c7c80f6
delabere <jack.rickards@hotmail.co.uk>
2019-11-24 12:44:13 +0000

    substituted in rugby data for test data

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ cf.go_offline()
 
 @app.route('/')
 def index():
-    data = pd.read_csv(r'data/test_data.csv')
+    data = pd.read_csv(r'data/rugby_sentiment_data.csv')
 
     figure_1 = data[['entity_sentiment','entity_name','entity_salience']]\
         .sort_values('entity_sentiment').iplot(kind='bar',
@@ -22,14 +22,14 @@ def index():
                                                 asFigure=True).to_html()
 
     figure_2 = data.iplot(
-        x='entity_sentiment',
-        y='entity_salience',
-        categories='entity_type',
-        xTitle='entity_sentiment',
-        yTitle='entity_salience',
-        title='Salience Vs Sentiment by Entity Type',
-        asFigure=True
-    ).to_html()
+                            x='entity_sentiment',
+                            y='entity_salience',
+                            categories='entity_type',
+                            xTitle='entity_sentiment',
+                            yTitle='entity_salience',
+                            title='Salience Vs Sentiment by Entity Type',
+                            asFigure=True
+                        ).to_html()
 
     charts = [figure_1, figure_2]
     return render_template('index.html', charts=charts)
